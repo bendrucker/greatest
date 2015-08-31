01da51c78859ea0c81e18aca62838a57cc8a940b
Ben Drucker <bvdrucker@gmail.com>
2015-08-31 12:04:57 -0700

    Actually fix sort

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict'
 
 var toArray = require('to-array')
-var sort = require('sort-on')
 
 module.exports = function greatest (values) {
   if (!Array.isArray(values)) {
     values = toArray(arguments)
   }
+  return values.slice().sort(order).reverse()[0]
+}
 
-  return sort(values).reverse()[0]
+function order (a, b) {
+  return a - b
 }
--- test.js
+++ test.js
@@ -6,5 +6,6 @@ var greatest = require('./')
 test(function (t) {
   t.equal(greatest([4, 5, 2, 8]), 8)
   t.equal(greatest(4, 5, 2, 8), 8)
+  t.equal(greatest(4, 2), 4)
   t.end()
 })
